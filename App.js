074f529ade19df3864ad02b293399eadedc80676
Amada Garcia <a01569104@tec.mx>
2025-08-14 17:47:30 -0600

    project initialization

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { AppBar } from "@react-native-material/core";
+import ConvertTemperatureForm from './app/ConvertTemperatureForm'
+
+export default function App() {
+    return (
+        <SafeAreaView spacing={5} style={styles.container}>
+        <AppBar title="Convertidor" />
+        <ConvertTemperatureForm/>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+});
